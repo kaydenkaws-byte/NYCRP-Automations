119c219717de32425c9c3a6fc900390f75af236d
kaydenkaws-byte <kaydenkaws@gmail.com>
2026-06-11 00:05:07 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Making Roleplay Immersive",
+        name: "Making Roleplay Immersive!",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 0, 
       },
     ],
   },
