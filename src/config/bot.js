9cd5f879a95c4cd01840ea8389f715d92fa4479b
kaydenkaws-byte <kaydenkaws@gmail.com>
2026-06-10 23:21:07 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -236,7 +236,7 @@ export const botConfig = {
     },
 
     // Default priority for new tickets.
-    defaultPriority: "none",
+    defaultPriority: "low",
 
     // Category ID where closed tickets are archived.
     archiveCategory: null,
